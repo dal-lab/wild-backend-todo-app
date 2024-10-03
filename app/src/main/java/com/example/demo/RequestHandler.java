07d98a027b009e326e34a0507e130ab2a1ed8849
jihwooon <jihwooon@gmail.com>
2024-10-03 14:02:13 +0900

    요청 본문을 읽는 기능 추가
    HttpExchange로부터 요청 본문을 읽어오는 기능을 추가하였습니다. 이를 통해 클라이언트로부터 전달받은 데이터를 처리할 수 있게 되었습니다.
    
    주요 변경사항:
    1. RequestHandler 클래스에 getRequestContent 메소드 추가: 이 메소드는 HttpExchange 객체를 인자로 받아, 요청 본문을 String 형태로 반환합니다.
    2. handle 메소드에서 getRequestContent 메소드를 호출하여 요청 본문을 읽어옵니다.

--- app/src/main/java/com/example/demo/RequestHandler.java
+++ app/src/main/java/com/example/demo/RequestHandler.java
@@ -3,13 +3,22 @@ package com.example.demo;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class RequestHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String responseContent = "Hello World";
+        String requestContent = getRequestContent(exchange);
+
+        String responseContent = requestContent;
+
         sendResponse(exchange, responseContent);
+    }
+
+    public String getRequestContent(HttpExchange exchange) throws IOException {
+        InputStream inputStream = exchange.getRequestBody();
+        return new String(inputStream.readAllBytes());
     }
 
     public void sendResponse(HttpExchange exchange, String responseContent)
