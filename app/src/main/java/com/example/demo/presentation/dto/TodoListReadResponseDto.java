5d85ec7c71299536df576f6d0aeb6e878ced9de9
GUAJEGUICHAN <qkr135qkr@gmail.com>
2024-10-03 18:23:23 +0900

    TodoListReadResponseDto 생성자 null 체크

--- app/src/main/java/com/example/demo/presentation/dto/TodoListReadResponseDto.java
+++ app/src/main/java/com/example/demo/presentation/dto/TodoListReadResponseDto.java
@@ -7,6 +7,9 @@ public class TodoListReadResponseDto {
     private final List<TodoCreateResponseDto> todoList;
 
     public TodoListReadResponseDto(List<TodoCreateResponseDto> todoList) {
+        if (todoList == null) {
+            throw new IllegalArgumentException("todoList must not be null");
+        }
         this.todoList = todoList;
     }
 
