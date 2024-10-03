160022e2686e210f4d5f3f99d41f239f72319f35
jihwooon <jihwooon@gmail.com>
2024-10-04 01:08:28 +0900

    Task 업데이트 로직 개선
    Task를 업데이트하는 로직에서 Optional 처리를 개선했습니다. 이전에는 Optional 객체를 먼저 얻고, 그것이 비어있는지 확인하는 방식이었지만, 이제는 orElseThrow를 사용하여 더 간결하게 처리합니다.
    
    주요 변경사항:
    1. TaskRepository에서 Task를 찾는 로직을 Optional의 orElseThrow를 사용하여 개선
    2. TaskNotFoundException이 발생할 수 있는 경우를 명확하게 표현

--- app/src/main/java/com/example/demo/application/TaskUpdator.java
+++ app/src/main/java/com/example/demo/application/TaskUpdator.java
@@ -4,19 +4,16 @@ import com.example.demo.exception.TaskNotFoundException;
 import com.example.demo.infrastructure.Task;
 import com.example.demo.infrastructure.TaskRepository;
 import com.example.demo.infrastructure.TaskRepositoryImp;
-import java.util.Optional;
 
 public class TaskUpdator {
 
     private final TaskRepository taskRepository = TaskRepositoryImp.getInstance();
 
     public Task updateTask(long id, String content) {
-        Optional<Task> optionalTask = taskRepository.findById(id);
-        if (optionalTask.isEmpty()) {
-            throw new TaskNotFoundException("해당 task를 찾을 수 없습니다.");
-        }
-
-        Task task = optionalTask.get();
+        Task task = taskRepository.findById(id)
+                .orElseThrow(
+                        () -> new TaskNotFoundException("해당 task를 찾을 수 없습니다.")
+                );
         task.setContent(content);
 
         return task;
