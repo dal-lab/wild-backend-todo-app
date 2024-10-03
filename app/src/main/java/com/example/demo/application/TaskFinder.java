ae6d2c6c1a1972f1658c6c3630a83e627e4b06cd
jihwooon <jihwooon@gmail.com>
2024-10-03 22:13:33 +0900

    Task 조회 및 업데이트 시 예외 처리 개선
    Task를 조회하거나 업데이트할 때 해당 Task가 존재하지 않는 경우에 대한 예외 처리를 개선하였습니다.
    
    주요 변경사항:
    1. TaskFinder와 TaskUpdator에서 Task를 찾을 수 없는 경우 기존에는 Error를 발생시켰지만, 이를 TaskNotFoundException으로 변경하였습니다.
    2. TaskNotFoundException 클래스를 새로 생성하였습니다. 이 예외는 Task를 찾을 수 없는 경우에 발생합니다.

--- app/src/main/java/com/example/demo/application/TaskFinder.java
+++ app/src/main/java/com/example/demo/application/TaskFinder.java
@@ -1,5 +1,6 @@
 package com.example.demo.application;
 
+import com.example.demo.exception.TaskNotFoundException;
 import com.example.demo.infrastructure.Task;
 import com.example.demo.infrastructure.TaskRepository;
 import com.example.demo.infrastructure.TaskRepositoryImp;
@@ -10,6 +11,6 @@ public class TaskFinder {
 
     public Task getTask(Long id) {
         return taskRepository.findById(id)
-                .orElseThrow(() -> new Error());
+                .orElseThrow(() -> new TaskNotFoundException("해당 task를 찾을 수 없습니다."));
     }
 }
--- app/src/main/java/com/example/demo/application/TaskUpdator.java
+++ app/src/main/java/com/example/demo/application/TaskUpdator.java
@@ -1,5 +1,6 @@
 package com.example.demo.application;
 
+import com.example.demo.exception.TaskNotFoundException;
 import com.example.demo.infrastructure.Task;
 import com.example.demo.infrastructure.TaskRepository;
 import com.example.demo.infrastructure.TaskRepositoryImp;
@@ -12,7 +13,7 @@ public class TaskUpdator {
     public Task updateTask(long id, String content) {
         Optional<Task> optionalTask = taskRepository.findById(id);
         if (optionalTask.isEmpty()) {
-            throw new Error("");
+            throw new TaskNotFoundException("해당 task를 찾을 수 없습니다.");
         }
 
         Task task = optionalTask.get();
