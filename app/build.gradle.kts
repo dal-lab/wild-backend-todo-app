d5fe5d506708c6edbe414300b302174c14cfc93b
GUAJEGUICHAN <qkr135qkr@gmail.com>
2024-10-04 19:35:56 +0900

    테스트 코드 작성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,10 @@ repositories {
 }
 
 dependencies {
+    testImplementation("io.rest-assured:rest-assured:4.4.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+
     implementation("org.apache.logging.log4j:log4j-core:2.24.1")
     implementation("org.apache.logging.log4j:log4j-api:2.24.1")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.18.0")
