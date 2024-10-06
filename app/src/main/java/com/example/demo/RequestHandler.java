fb5665f58c0762d364679ee13abd9a79277044f2
jihwooon <jihwooon@gmail.com>
2024-10-06 12:31:59 +0900

    RequestHandler에서 불필요한 리소스 제거
    RequestHandler 클래스에서 사용하지 않는 리소스를 제거하였습니다. 이로 인해 코드의 가독성이 향상되었습니다.
    
    주요 변경사항:
    1. CreateTaskResource, FindTaskResource, HomeResource, ListTaskResource, RemoveTaskResource, UpdateTaskResource 등의 import문 제거
    2. getResponseContent 메소드에서 위 리소스들을 사용하는 코드 제거

--- app/src/main/java/com/example/demo/RequestHandler.java
+++ app/src/main/java/com/example/demo/RequestHandler.java
@@ -1,11 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.controller.CreateTaskResource;
-import com.example.demo.controller.FindTaskResource;
-import com.example.demo.controller.HomeResource;
-import com.example.demo.controller.ListTaskResource;
-import com.example.demo.controller.RemoveTaskResource;
-import com.example.demo.controller.UpdateTaskResource;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
@@ -28,38 +22,5 @@ public class RequestHandler implements HttpHandler {
         }
 
         new ResponseSuccess(exchange).send(responseContent);
-    }
-
-    private String getResponseContent(String requestMethod, String requestUri,
-            String requestContent) throws IOException {
-        if (requestMethod.equals("GET") && requestUri.equals("/")) {
-            return new HomeResource().handler();
-        }
-
-        if (requestMethod.equals("POST") && requestUri.equals("/tasks")) {
-            return new CreateTaskResource().handler(requestContent);
-        }
-
-        if (requestMethod.equals("GET") && requestUri.equals("/tasks")) {
-            return new ListTaskResource().handler();
-        }
-
-        if (requestMethod.equals("GET") && requestUri.startsWith("/tasks/")) {
-            Long taskId = new TaskPathId().getPathId(requestUri);
-            return new FindTaskResource().handler(taskId);
-        }
-
-        if (requestMethod.equals("PATCH") && requestUri.startsWith("/tasks/")) {
-            Long taskId = new TaskPathId().getPathId(requestUri);
-            return new UpdateTaskResource().handler(taskId, requestContent);
-        }
-
-        if (requestMethod.equals("DELETE") && requestUri.startsWith(
-                "/tasks/")) {
-            Long taskId = new TaskPathId().getPathId(requestUri);
-            return new RemoveTaskResource().handler(taskId);
-        }
-
-        return null;
     }
 }
