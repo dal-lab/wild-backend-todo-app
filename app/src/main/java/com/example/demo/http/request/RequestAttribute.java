61dc6d5f7aba92d80a1eda93cb865acb3c6f8c2e
jihwooon <jihwooon@gmail.com>
2024-10-09 13:33:59 +0900

    HTTP 요청/응답 처리 관련 클래스 제거
    이 커밋에서는 HTTP 요청과 응답을 처리하는 여러 클래스들을 제거했습니다. 이러한 변경은 프로젝트의 구조를 단순화하고, 유지보수를 용이하게 하기 위한 것입니다.
    
    주요 변경사항:
    1. HTTP 요청을 처리하는 `RequestHandler`, `RequestBody`, `RequestAttribute` 등의 클래스 제거
    2. HTTP 응답을 처리하는 `Response`, `ResponseHandler`, `ResponseContent` 등의 클래스 제거
    3. HTTP 메소드(`GET`, `POST`, `PATCH`, `DELETE`)를 처리하는 클래스 제거
    4. 요청 내용을 처리하는 `RequestHandlerStrategy` 인터페이스와 이를 구현하는 여러 클래스 제거
    5. Task 생성, 조회, 업데이트, 삭제 등을 처리하는 `CreateTaskResource`, `FindTaskResource`, `UpdateTaskResource`, `RemoveTaskResource` 등의 클래스 제거

--- app/src/main/java/com/example/demo/http/request/RequestAttribute.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.http.request;
-
-public record RequestAttribute(
-        String requestContent,
-        String requestMethod,
-        String requestURI
-) {
-
-}
--- app/src/main/java/com/example/demo/http/requestContent/RequestHandlerStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.http.requestContent;
-
-import com.example.demo.http.request.RequestAttribute;
-import java.io.IOException;
-
-public interface RequestHandlerStrategy {
-    boolean matches(RequestAttribute requestAttribute);
-    String handle(RequestAttribute requestAttribute) throws IOException;
-}
