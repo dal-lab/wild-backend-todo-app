bf210b1659acbf6899590c757dfe1b051971312d
jihwooon <jihwooon@gmail.com>
2024-10-10 00:47:23 +0900

    TaskRemover와 관련 테스트 코드에서 예외 클래스 변경
    TaskNotFoundException이 TaskIdNotFoundException으로 변경되었습니다. 이에 따라 관련 코드와 테스트 코드를 수정하였습니다.
    
    주요 변경사항:
    1. TaskRemover에서 TaskNotFoundException을 던지던 부분을 TaskIdNotFoundException을 던지도록 수정하였습니다.
    2. TaskRemoverTest에서 TaskNotFoundException을 import하던 부분을 제거하였습니다.
    3. TaskRemoverTest에서 테스트 케이스를 수정하여 TaskIdNotFoundException이 정상적으로 발생하는지 확인하였습니다.

--- app/src/test/java/com/example/demo/application/TaskRemoverTest.java
+++ app/src/test/java/com/example/demo/application/TaskRemoverTest.java
@@ -6,7 +6,6 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
 import com.example.demo.exception.TaskIdNotFoundException;
-import com.example.demo.exception.TaskNotFoundException;
 import com.example.demo.infrastructure.Task;
 import com.example.demo.infrastructure.TaskRepository;
 import java.util.Optional;
@@ -19,29 +18,33 @@ class TaskRemoverTest {
 
     private TaskRepository taskRepository;
 
+    private static Long existentTaskId = 1L;
+    private static Long nonExistentTaskId = 9999L;
+
     @BeforeEach
     void setUp() {
         taskRepository = mock(TaskRepository.class);
         taskRemover = new TaskRemover(taskRepository);
 
-        given(taskRepository.findById(1L)).willReturn(
-                Optional.of(new Task(1L, "오늘 할 일")));
+        given(taskRepository.findById(existentTaskId)).willReturn(
+                Optional.of(new Task(existentTaskId, "오늘 할 일")));
 
-        given(taskRepository.findById(9999L)).willReturn(Optional.empty());
+        given(taskRepository.findById(nonExistentTaskId)).willReturn(
+                Optional.empty());
 
-        given(taskRepository.remove(1L)).willReturn(true);
+        given(taskRepository.remove(existentTaskId)).willReturn(true);
     }
 
     @Test
     void shouldReturnTrueIfRemoveSuccess() {
-        boolean isRemoveTask = taskRemover.removeTask(1L);
+        boolean isRemoveTask = taskRemover.removeTask(existentTaskId);
 
         assertThat(isRemoveTask).isTrue();
     }
 
     @Test
     void shouldReturnFalseIfRemoveFail() {
-        assertThatThrownBy(() -> taskRemover.removeTask(9999L))
+        assertThatThrownBy(() -> taskRemover.removeTask(nonExistentTaskId))
                 .isInstanceOf(TaskIdNotFoundException.class)
                 .hasMessage("Task 9999를 찾을 수 없습니다.");
     }
