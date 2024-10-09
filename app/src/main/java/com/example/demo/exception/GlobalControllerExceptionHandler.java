2f4c61014509129054110cf1e2655a2a29055331
jihwooon <jihwooon@gmail.com>
2024-10-10 00:20:57 +0900

    전역 예외 처리기 및 ErrorResponse 추가
    이 커밋에서는 전역 예외 처리기와 ErrorResponse를 추가했습니다. 이를 통해 애플리케이션에서 발생하는 예외를 효과적으로 관리하고 사용자에게 적절한 에러 메시지를 제공할 수 있습니다.
    
    주요 변경사항:
    1. ErrorResponse 클래스 추가: 에러 메시지를 담는 클래스입니다.
    2. GlobalControllerExceptionHandler 클래스 추가: 애플리케이션에서 발생하는 예외를 전역적으로 처리하는 클래스입니다. 현재는 TaskNotFoundException을 처리하는 메소드를 포함하고 있습니다.

---
+++ app/src/main/java/com/example/demo/exception/GlobalControllerExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseBody
+@ControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ErrorResponse handleTaskNotFound() {
+        return new ErrorResponse("해당 Task 정보를 찾을 수 없습니다");
+    }
+
+}
